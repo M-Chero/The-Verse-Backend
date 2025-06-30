fbcd74a54c21ad7ec7a30f9a60e2a6f9a90ca99c
M-Chero <mitchiemonroe43@gmail.com>
2025-06-30 09:26:41 +0300

    Update: Deploy-Clean

--- models/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class ClaimAmountDensityItem(BaseModel):
-    claim_amount: float
-    Gold: float
-    Platinum: float
-    Silver: float
--- routers/v1_routers.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from api.v1 import upload, clean, claims
-
-v1_router = APIRouter()
-
-v1_router.include_router(upload.router, prefix="/v1", tags=["upload"])
-v1_router.include_router(clean.router, prefix="/v1", tags=["clean"])
-v1_router.include_router(claims.router, prefix="/v1", tags=["claims"])
