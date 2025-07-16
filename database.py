f750e9c593056900a1445f1c055bc38b68cbe6bc
M-Chero <mitchiemonroe43@gmail.com>
2025-07-16 03:15:07 +0300

    Deploy

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import URL
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+load_dotenv()
 
 url_object = URL.create(
     drivername=os.getenv("DB_CONNECTION", "mysql") + "+pymysql",
